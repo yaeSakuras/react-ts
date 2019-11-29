cf2b741468f806543e704acf0e8a98e8c0c32808
Zhou <363420113@qq.com>
2019-11-29 05:36:43 +0000

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hello world
         </a>
       </header>
     </div>
