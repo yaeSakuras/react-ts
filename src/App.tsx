a41b019acb63189c3c7841c6e06fbbd9090af21a
zhouzh <363420113@qq.com>
2019-12-09 09:57:31 +0800

    升级依赖

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter,Switch } from "react-router-dom";
 import RouteLoading from './components/Route/RouteLoading';
 import RouteWithSubRoutes from './components/Route/RouteWithSubRoutes';
 import routes from './routerConfig';
+
 const App: React.FC = () => {
     return (
         <HashRouter>
